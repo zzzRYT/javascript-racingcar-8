b5e4dac0c3847857bf6bb527bb927ec9c135e1c2
zzzryt <jinjinstar3@gmail.com>
2025-10-22 13:53:44 +0900

    test(Car): 자동차 이름 관련 테스트 추가

--- __tests__/Car.spec.js
+++ __tests__/Car.spec.js
@@ -1,13 +1,26 @@
+import { MAX_LENGTH_CAR_NAME } from '../constants';
 import { Car } from '../src/domain/Car';
 
 describe('Car Class 단위 테스트', () => {
   let car;
 
   beforeEach(() => {
-    car = new Car();
+    car = new Car('pobi');
   });
 
-  test('무작위 수가 4 이상일 경우 전진한다.', () => {});
+  test('자동차 이름은 5자 이하여야 한다.', () => {
+    const nameLength = car.name.length;
+    expect(nameLength).toBeLessThanOrEqual(MAX_LENGTH_CAR_NAME);
+  });
+
+  test('자동차 이름이 5자를 초과할 경우 에러를 발생시킨다.', () => {
+    const longName = 'pobipobi';
+    expect(() => new Car(longName)).toThrow();
+  });
+
+  test('무작위 수가 4 이상일 경우 전진한다.', () => {
+    const greaterThanFour = 5;
+  });
 
   test('무작위 수가 4 미만일 경우 정지한다.', () => {});
 });
--- constants.js
+++ constants.js
@@ -1,1 +1,3 @@
 export const WINNER_DESCRIPTION = '최종 우승자';
+
+export const MAX_LENGTH_CAR_NAME = 5;
